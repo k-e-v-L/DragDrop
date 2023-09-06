963d3907096064ff2ba48c158ad932788493fe0b
M Kael <kevliiver@gmail.com>
2023-09-06 14:52:16 +0300

    DragPanel added listeners + final WIDTH/HEIGHT.

--- DragPanel.java
+++ DragPanel.java
@@ -1,4 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionAdapter;
 
 public class DragPanel extends JPanel {
+
+    ImageIcon image = new ImageIcon("Naeratus.png");
+    final int WIDTH = image.getIconWidth(); //final beacause we are not planning on changing the size
+    final int HEIGHT = image.getIconHeight(); //final beacause we are not planning on changing the size
+    DragPanel(){
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    private class ClickListener extends MouseAdapter{
+
+    }
+    private class DragListener extends MouseMotionAdapter{
+
+    }
 }
--- MyFrame.java
+++ MyFrame.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
-
     DragPanel dragPanel = new DragPanel();
     MyFrame(){
 
+        this.add(dragPanel);
         this.setTitle("Drag & Drop");
         this.setSize(500,500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
